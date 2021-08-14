18d643bc40491b17cfbb9dc31f467c707e0ea789
Duc Thinh <thinhapcs@gmail.com>
2021-08-14 22:44:07 +0700

    add API for admin

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,111 +1,165 @@
-const Products = require('../models/productModel')
+const Products = require("../models/productModel");
 
 // Filter, sorting and paginating
 
 class APIfeatures {
-    constructor(query, queryString){
-        this.query = query;
-        this.queryString = queryString;
-    }
-    filtering(){
-       const queryObj = {...this.queryString} //queryString = req.query
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+  filtering() {
+    const queryObj = { ...this.queryString }; //queryString = req.query
 
-       const excludedFields = ['page', 'sort', 'limit']
-       excludedFields.forEach(el => delete(queryObj[el]))
-       
-       let queryStr = JSON.stringify(queryObj)
-       queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
+    const excludedFields = ["page", "sort", "limit"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lt|lte|regex)\b/g,
+      (match) => "$" + match
+    );
 
     //    gte = greater than or equal
     //    lte = lesser than or equal
     //    lt = lesser than
     //    gt = greater than
-       this.query.find(JSON.parse(queryStr))
-         
-       return this;
+    this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sorting() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("-createdAt");
     }
 
-    sorting(){
-        if (this.queryString.sort){
-            const sortBy = this.queryString.sort.split(',').join(' ')
-            this.query = this.query.sort(sortBy)
-        }else{
-            this.query = this.query.sort('-createdAt')
-        }
+    return this;
+  }
 
-        return this;
-    }
-
-    paginating(){
-        const page = this.queryString.page * 1 || 1
-        const limit = this.queryString.limit * 1 || 9
-        const skip = (page - 1) * limit;
-        this.query = this.query.skip(skip).limit(limit)
-        return this;
-    }
+  paginating() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 9;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
 }
 
 const productCtrl = {
-    getProducts: async(req, res) =>{
-        try {
-            const features = new APIfeatures(Products.find(), req.query)
-            .filtering().sorting().paginating()
+  getProducts: async (req, res) => {
+    try {
+      // const features = new APIfeatures(Products.find(), req.query)
+      //   .filtering()
+      //   .sorting()
+      //   .paginating();
 
-            const products = await features.query
+      // const products = await features.query;
+      const products = await Products.find({});
 
-            res.json({
-                status: 'success',
-                result: products.length,
-                products: products
-            })
-            
-        } catch (err) {
-            return res.status(500).json({msg: err.message})
-        }
-    },
-    createProduct: async(req, res) =>{
-        try {
-            const {id, name, cost, author, publishedYear, picture, category, quantity, onDiscount, numOfReviews, ratePoint} = req.body;
-            if (!picture) return res.status(400).json({msg: "No image upload"})
-
-            const product = await Products.findOne({id})
-            if (product)
-                return res.status(400).json({msg: "This product already exists."})
-
-            const newProduct = new Products({
-                id, name: name.toLowerCase(), cost, author, publishedYear, picture, category, quantity, onDiscount, numOfReviews, ratePoint
-            })
-
-            await newProduct.save()
-            res.json({msg: "Created a product"})
-
-        } catch (err) {
-            return res.status(500).json({msg: err.message})
-        }
-    },
-    deleteProduct: async(req, res) =>{
-        try {
-            await Products.findByIdAndDelete(req.params.id)
-            res.json({msg: "Deleted a Product"})
-        } catch (err) {
-            return res.status(500).json({msg: err.message})
-        }
-    },
-    updateProduct: async(req, res) =>{
-        try {
-            const {name, cost, author, publishedYear, picture, category, quantity, onDiscount, numOfReviews, ratePoint} = req.body;
-            if (!picture) return res.status(400).json({msg: "No image upload"})
-
-            await Products.findOneAndUpdate({_id: req.params.id}, {
-                name: name.toLowerCase(), cost, author, publishedYear, picture, category, quantity, onDiscount, numOfReviews, ratePoint
-            })
-
-            res.json({msg: "Updated a Product"})
-        } catch (err) {
-            return res.status(500).json({msg: err.message})
-        }
+      res.json({
+        status: "success",
+        result: products.length,
+        products: products,
+      });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
     }
-}
+  },
+  createProduct: async (req, res) => {
+    try {
+      const {
+        id,
+        name,
+        cost,
+        author,
+        publishedYear,
+        picture,
+        category,
+        quantity,
+        onDiscount,
+        numOfReviews,
+        ratePoint,
+      } = req.body;
+      if (!picture) return res.status(400).json({ msg: "No image upload" });
 
+      const product = await Products.findOne({
+        name: name.toLowerCase(),
+        author: author,
+        publishedYear: publishedYear,
+        cost: cost,
+      });
+      if (product)
+        return res.status(400).json({ msg: "This product already exists." });
 
-module.exports = productCtrl
+      const newProduct = new Products({
+        id,
+        name: name.toLowerCase(),
+        cost,
+        author,
+        publishedYear,
+        picture,
+        category,
+        quantity,
+        onDiscount,
+        numOfReviews,
+        ratePoint,
+      });
+
+      await newProduct.save();
+      res.json({ msg: "Created a product" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  deleteProduct: async (req, res) => {
+    console.log(req.params.id);
+    try {
+      await Products.findByIdAndDelete(req.params.id);
+      res.json({ msg: "Deleted a Product" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  updateProduct: async (req, res) => {
+    try {
+      const {
+        name,
+        cost,
+        author,
+        publishedYear,
+        picture,
+        category,
+        quantity,
+        onDiscount,
+        numOfReviews,
+        ratePoint,
+      } = req.body;
+      if (!picture) return res.status(400).json({ msg: "No image upload" });
+
+      await Products.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          name: name.toLowerCase(),
+          cost,
+          author,
+          publishedYear,
+          picture,
+          category,
+          quantity,
+          onDiscount,
+          numOfReviews,
+          ratePoint,
+        }
+      );
+
+      res.json({ msg: "Updated a Product" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+
+module.exports = productCtrl;
--- server.js
+++ server.js
@@ -1,48 +1,58 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const morgan = require('morgan')
-const fileUpload = require('express-fileupload')
-const cookieParser = require('cookie-parser')
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
+const app = express();
 
-const app = express()
-
-app.use(express.json())
-app.use(cookieParser())
-app.use(morgan('tiny'))
-app.use(cors({
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("tiny"));
+app.use(
+  cors({
     origin: ["http://localhost:3000"],
-    method: ["GET", "POST"]
-}))
-app.use(fileUpload({
-    useTempFiles: true
-}))
+    method: ["GET", "POST"],
+  })
+);
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 
 // Routes
-app.use('/user', require('./routes/userRouter'))
-app.use('/api', require('./routes/categoryRouter'))
-app.use('/api', require('./routes/upload'))
-app.use('/api', require('./routes/productRouter'))
-// app.use('/api', require('./routes/paymentRouter'))
+app.use("/user", require("./routes/user/userRouter"));
+app.use("/user", require("./routes/user/categoryRouter"));
+app.use("/user", require("./routes/user/productRouter"));
+// app.use("/user", require("./routes/user/upload"));
+// app.use('/user', require('./routes/paymentRouter'))
 
-
+app.use("/admin", require("./routes/admin/userRouter"));
+app.use("/admin", require("./routes/admin/categoryRouter"));
+app.use("/admin", require("./routes/admin/upload"));
+app.use("/admin", require("./routes/admin/productRouter"));
 
 // Connect to mongodb
-const URI = process.env.MONGODB_URL
-mongoose.connect(URI, {
+const URI = process.env.MONGODB_URL;
+mongoose.connect(
+  URI,
+  {
     useCreateIndex: true,
     useFindAndModify: false,
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}, err =>{
+    useUnifiedTopology: true,
+  },
+  (err) => {
     if (err) throw err;
-    console.log('Connected to MongoDB')
-})
+    console.log("Connected to MongoDB");
+  }
+);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () =>{
-    console.log('Server is running on port', PORT)
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
